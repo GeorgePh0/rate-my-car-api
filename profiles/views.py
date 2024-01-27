78ec8bb58caa2cd744fafccec9a869eb1cf5c936
GeorgePh0 <george.philip0020@gmail.com>
2024-01-27 17:05:38 +0000

    Add number count to profiles, seeing number of posts, followers and following

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics
 from rmc_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
@@ -6,7 +7,11 @@ from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
 
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 
