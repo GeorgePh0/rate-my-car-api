e8810011f3689a75bbd044d091b0ddc0beea8db1
GeorgePh0 <george.philip0020@gmail.com>
2024-01-30 11:38:33 +0000

    Fixed heroku deployment

--- rmc_api/settings.py
+++ rmc_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get(ALLOWED_HOST), '8000-georgeph0-ratemycarapi-106kent4iej.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '8000-georgeph0-ratemycarapi-106kent4iej.ws-eu107.gitpod.io']
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
